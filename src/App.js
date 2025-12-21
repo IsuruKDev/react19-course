520027b58c65dbff98357e98ed891414d76cd49f
Isuru <isurujr.onine@gmail.com>
2025-12-21 08:45:25 +0530

    init chatbot project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to React 19 course...</h2>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-      <button className="button-4" role="button">Button 4</button>
-
-      <br/>
-      <Card/>      
     </div>
   );
 }
